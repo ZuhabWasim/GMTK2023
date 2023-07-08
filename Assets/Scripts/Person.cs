c60296d64542a42f0751182010e52e89ad891814
Zuhab Wasim <z-wasim-786@live.com>
2023-07-08 12:12:18 -0400

    Cleaned up dialoguebox + person animating logic

--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -2,9 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Types;
+using System;
 
 public class Person : MonoBehaviour
 {
     public string characterName;
     public Emotion emotion;
+
+    [SerializeField]
+    private PersonAnimator personAnimator;
+
+    void Start()
+    {
+        DialogueManager dm = FindObjectOfType<DialogueManager>();
+        if (dm)
+            dm.PersonEmote += CatchEmote;
+    }
+
+    private void CatchEmote(Person person, Emotion emotion)
+    {
+        if (person != this)
+            return;
+
+        this.emotion = emotion;
+        OnEmote();
+    }
+
+    public virtual void OnEmote()
+    {
+        personAnimator.UpdateSprite(emotion);
+    }
 }
