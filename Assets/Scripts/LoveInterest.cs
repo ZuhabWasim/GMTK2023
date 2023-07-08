ecbce248a8fbe8bab4c537afd9e1c4b6ae365676
Albert Cao <albert.cao.1000@gmail.com>
2023-07-08 02:28:08 -0400

    Can now save character state

---
+++ Assets/Scripts/LoveInterest.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Types;
+
+public class LoveInterest : Person
+{
+    public int affection;
+
+    public void SaveLoveInterest() {
+        SaveSystem.SaveLoveInterest(this);
+    }
+
+    public void LoadLoveInterest(string name) {
+        LoveInterestData data = SaveSystem.LoadLoveInterest(name);
+
+        this.characterName = data.characterName;
+        this.affection = data.affection;
+        this.emotion = (Emotion)data.emotion; 
+    }
+
+    public void Reset() {
+        SaveSystem.ClearPersistentData();
+    }
+}
---
+++ Assets/Scripts/LoveInterestData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LoveInterestData
+{
+    public string characterName;
+    public int affection;
+    public int emotion; // has to be a primitive
+
+    public LoveInterestData(LoveInterest partner) {
+        // if classes other than LoveInterest inherit Person, then
+        // move characterName and affection to another function
+        characterName = partner.characterName;
+        affection = partner.affection;
+        emotion = (int) partner.emotion;
+    }
+}
