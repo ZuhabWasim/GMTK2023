3539ed1fb4b5fd49292829bc96fea96befc1b1b4
Albert Cao <albert.cao.1000@gmail.com>
2023-07-08 22:41:38 -0400

    completed audio system

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+    public static AudioManager instance;
+
+    void Awake()
+    {
+
+        if (instance == null) { instance = this; }
+        else { 
+            Destroy(gameObject); 
+            return; 
+        }
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds) {
+            s.source = gameObject.AddComponent<AudioSource>();
+
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    void Start() {
+        Play("burger_king");
+        // Play("piano_track");
+    }
+
+    // If you want to play audio statically:
+    // FindObjectOfType<AudioManager>().Play("AUDIO-NAME");
+    public void Play (string target) {
+        Sound s = Array.Find(sounds, x => x.name == target);
+        if (s == null) { 
+            Debug.Log("Cant find sound: " + target);
+            return; 
+        }
+        s.source.Play();
+    }
+}
